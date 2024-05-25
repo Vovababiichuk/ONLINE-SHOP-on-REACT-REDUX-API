8fbc4c50d20b2208c7b2672ed05cfd9a176c4658
VVVV BBBB <Volodymyr>
2024-05-25 07:43:20 +0300

    sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,21 @@
-
+import { NavLink } from 'react-router-dom'
+import styles from './Sidebar.module.css';
+// import { ROUTES } from '../../utils/routes'
 export const Sidebar = () => {
 	return (
-		<div>Sidebar</div>
-	)
-}
+		<section className={styles.sidebar}>
+			<h2 className={styles.title}>Categories</h2>
+			<nav>
+				<ul className={styles.menu}>
+					<NavLink to={`/categories/${1}`}>
+						Link
+					</NavLink>
+				</ul>
+			</nav>
+			<div className={styles.sidebarFooter}>
+				<a href="/help" target='_blank' className={styles.link}>Help</a>
+				<a href="/terms" target='_blank' className={styles.link}>Terms & Conditions</a>
+			</div>
+		</section>
+	);
+};
