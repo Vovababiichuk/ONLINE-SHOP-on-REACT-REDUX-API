ada58f0a082b6327ff5866d62de24aa7125db055
VVVV BBBB <Volodymyr>
2024-05-26 16:02:19 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,23 +3,26 @@ import styles from './Header.module.css';
 import { ROUTES } from '../../utils/routes';
 import LOGO from '../../assets/images/logo2.svg';
 import AVATAR from '../../assets/images/avatar.svg';
-import { FaSearch, FaRegHeart  } from "react-icons/fa";
-import { ShoppingBag } from 'lucide-react'
+import { FaSearch, FaRegHeart } from 'react-icons/fa';
+import { ShoppingBag } from 'lucide-react';
 export const Header = () => {
 	return (
 		<header className={styles.header}>
+
 			<div className={styles.logo}>
 				<Link to={ROUTES.HOME}>
 					<img src={LOGO} alt='logo stuff' />
 				</Link>
 			</div>
-			<div className={styles.info}>
-				<div className={styles.user}>
-					<div className={styles.avatar}>
-						<img className={styles.avatarImg} src={AVATAR} alt="avatar" />
-					</div>
-					<div className={styles.username}>Guest</div>
+
+			<div className={styles.user}>
+				<div className={styles.avatar}>
+					<img className={styles.avatarImg} src={AVATAR} alt='avatar' />
 				</div>
+				<div className={styles.username}>Guest</div>
+			</div>
+
+			<div className={styles.info}>
 				<form className={styles.form}>
 					<div className={styles.icon}>
 						<FaSearch className={styles.iconSearch} />
@@ -36,16 +39,18 @@ export const Header = () => {
 					</div>
 					{false && <div className={styles.box}></div>}
 				</form>
-				<div className={styles.account}>
-					<Link to={ROUTES.HOME} className={styles.favorites}>
-						<FaRegHeart className={styles.icon} />
-					</Link>
-					<Link to={ROUTES.CART} className={styles.cart}>
-						<ShoppingBag className={styles.icon} />
-						<span className={styles.count}>2</span>
-					</Link>
-				</div>
 			</div>
+
+			<div className={styles.account}>
+				<Link to={ROUTES.HOME} className={styles.favorites}>
+					<FaRegHeart className={styles.icon} />
+				</Link>
+				<Link to={ROUTES.CART} className={styles.cart}>
+					<ShoppingBag className={styles.icon} />
+					<span className={styles.count}>2</span>
+				</Link>
+			</div>
+
 		</header>
 	);
 };
