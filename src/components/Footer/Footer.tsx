3c240bf8b78d0d1957d5c19b84c64cc6701f70a0
VVVV BBBB <Volodymyr>
2024-05-30 20:55:46 +0300

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,36 +8,49 @@ import { IoLogoInstagram } from 'react-icons/io';
 
 export const Footer = () => {
 	return (
-		<footer className={styles.footer}>
-			<div className={styles.logo}>
-				<Link to={ROUTES.HOME}>
-					<img src={LOGO} alt='logo stuff' />
-				</Link>
-			</div>
-
-			<div className={styles.rights}>
-				Developed by{' '}
-				<a
-					href='https://volodymyrcodepro.site/'
-					target='_blank'
-					rel='noreferrer'
-				>
-					VOLODYMYR CODE_PRO
-				</a>
-			</div>
-
-			<div className={styles.socials}>
-				<a href='#' target='_blank' rel='noreferrer' onClick={(e) => e.preventDefault()}>
-					<CiYoutube className={styles.icon} />
-				</a>
-
-				<a href='#' target='_blank' rel='noreferrer' onClick={(e) => e.preventDefault()}>
-					<CiFacebook className={styles.icon} />
-				</a>
-
-				<a href='#' target='_blank' rel='noreferrer' onClick={(e) => e.preventDefault()}>
-					<IoLogoInstagram className={styles.icon} />
-				</a>
+		<footer className={styles.footerWrap}>
+			<div className={styles.footer}>
+				<div className={styles.logo}>
+					<Link to={ROUTES.HOME}>
+						<img src={LOGO} alt='logo stuff' />
+					</Link>
+				</div>
+				<div className={styles.rights}>
+					Developed by{' '}
+					<a
+						href='https://volodymyrcodepro.site/'
+						target='_blank'
+						rel='noreferrer'
+					>
+						VOLODYMYR CODE_PRO
+					</a>
+				</div>
+				<div className={styles.socials}>
+					<a
+						href='#'
+						target='_blank'
+						rel='noreferrer'
+						onClick={e => e.preventDefault()}
+					>
+						<CiYoutube className={styles.icon} />
+					</a>
+					<a
+						href='#'
+						target='_blank'
+						rel='noreferrer'
+						onClick={e => e.preventDefault()}
+					>
+						<CiFacebook className={styles.icon} />
+					</a>
+					<a
+						href='#'
+						target='_blank'
+						rel='noreferrer'
+						onClick={e => e.preventDefault()}
+					>
+						<IoLogoInstagram className={styles.icon} />
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
