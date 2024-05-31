75516d9d6124152f4e29edf50ccbed9f0241473d
VVVV BBBB <Volodymyr>
2024-05-31 13:16:00 +0300

    update photo categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -12,7 +12,7 @@ type RootState = {
 
 const categoryImages: Record<string, string> = {
   electronics: 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/5941571022b7bae7255eadea82977496.jpg?imageView2/2/w/800/q/70/format/webp',
-  jewelery: 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/5fee3503001bb661a68a91001bd5270e.jpg?imageView2/2/w/800/q/70/format/webp',
+  jewelery: 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/a2f61957b3652cacf2a7a80e458203fd.jpg?imageView2/2/w/800/q/70/format/webp',
   "men's clothing": 'https://img.kwcdn.com/product/fancy/decf20ac-b8a7-4509-b5ed-f961f858b54e.jpg?imageView2/2/w/800/q/70/format/webp',
   "women's clothing": 'https://img.kwcdn.com/product/fancy/90909ca0-a69d-4a6a-9bf7-c1ca99ae5ce8.jpg?imageView2/2/w/800/q/70/format/webp',
 };
